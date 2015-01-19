6f2d90cffe6eb126a3de2f31493d2c7279977888
fabien.lassie@gmail.com <fabien.lassie@gmail.com@826a27cf-b9c3-140f-935a-f17cb0f93c71>
2015-01-19 15:28:40 +0000

    Add function erose and dilate By Sofiane And Fabien

--- src/server/imageprocessing/processing/MorphologicalFilter.java
+++ src/server/imageprocessing/processing/MorphologicalFilter.java
@@ -3,40 +3,123 @@
  */
 package server.imageprocessing.processing;
 
+import java.awt.Color;
 import java.awt.Image;
+import java.awt.image.BufferedImage;
 
 /**
+ * ..
+ * 
  * @author Thomas
  *
  */
-class MorphologicalFilter 
-{
+public final class MorphologicalFilter {
+	
+	/**
+	 * ..
+	 */
+	private MorphologicalFilter() {
+		
+	}
+
 
 	/**
+	 * ..
 	 * 
+	 * @param image
+	 * ..
+	 * @param coefficient
+	 * ..
+	 * @return
+	 * ..
 	 */
-	public MorphologicalFilter() 
-	{
+	public static Image opening(final Image image, final int coefficient) {
+		return dilatate(erose(image, coefficient), coefficient);
 	}
 
-	static Image opening(Image image, int coefficient)
-	{
-		return null;
+	/**
+	 * ..
+	 * 
+	 * @param image
+	 * ..
+	 * @param coefficient
+	 * ..
+	 * @return
+	 * ..
+	 */
+	public static Image closing(final Image image, final int coefficient) {
+		return erose(dilatate(image, coefficient), coefficient);
 	}
 
-	static Image closing(Image image, int coefficient)
-	{
-		return null;
+	/**
+	 * ..
+	 * 
+	 * @param image
+	 * ..
+	 * @param coefficient
+	 * ..
+	 * @return
+	 * ..
+	 */
+	public static Image erose(final Image image, final int coefficient) {
+		BufferedImage b = new BufferedImage(image.getWidth(null), image.getHeight(null), BufferedImage.TYPE_BYTE_BINARY);
+        b.createGraphics().drawImage(image, 0, 0, null);
+            
+        BufferedImage bnew = new BufferedImage(image.getWidth(null), image.getHeight(null), BufferedImage.TYPE_BYTE_BINARY);
+        
+		for (int i = coefficient; i < b.getWidth() - coefficient; i++) {
+			for (int j = coefficient; j < b.getHeight() - coefficient; j++) {
+				boolean a = false;
+				for (int k = -coefficient; k <= coefficient; k++) {
+					for (int l = -coefficient; l <= coefficient; l++) {	
+						if (b.getRGB(i + k, j + l) == Color.WHITE.getRGB()) {
+							a = true; 
+						}
+					}
+				}
+				if (a) {
+					bnew.setRGB(i, j, Color.WHITE.getRGB());
+				} else {
+					bnew.setRGB(i, j, Color.BLACK.getRGB());
+				}
+			}
+		}
+		return bnew;
 	}
 
-
-	static Image erose(Image image, int coefficient)
-	{
-		return null;
-	}
-
-	static Image dilatate(Image image, int coefficient)
-	{
-		return null;
+	/**
+	 * ..
+	 * 
+	 * @param image
+	 * ..
+	 * @param coefficient
+	 * ..
+	 * @return
+	 * ..
+	 */
+	public static Image dilatate(final Image image, final int coefficient) {
+		BufferedImage b = new BufferedImage(image.getWidth(null), image.getHeight(null), BufferedImage.TYPE_BYTE_BINARY);
+        b.createGraphics().drawImage(image, 0, 0, null);
+            
+        BufferedImage bnew = new BufferedImage(image.getWidth(null), image.getHeight(null), BufferedImage.TYPE_BYTE_BINARY);
+        
+        for (int i = coefficient; i < b.getWidth() - coefficient; i++) {
+			for (int j = coefficient; j < b.getHeight() - coefficient; j++) {
+				boolean a = false;
+				for (int k = -coefficient; k <= coefficient; k++) {
+					for (int l = -coefficient; l <= coefficient; l++) {	
+						if (b.getRGB(i + k, j + l) == Color.BLACK.getRGB()) {
+							a = true; 
+						}
+					}
+				}
+				if (a) {
+					bnew.setRGB(i, j, Color.BLACK.getRGB());
+				} else {
+					bnew.setRGB(i, j, Color.WHITE.getRGB());
+				}
+			}
+		}
+		return bnew;
 	}
 }
