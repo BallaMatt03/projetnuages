e97c7fbbc2ef2db339462f2c30e631bf37e6582c
tomavron94@gmail.com <tomavron94@gmail.com@826a27cf-b9c3-140f-935a-f17cb0f93c71>
2015-01-19 12:41:50 +0000

    create template class

---
+++ trunk/src/server/imageprocessing/processing/MorphologicalFilter.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package server.imageprocessing.processing;
+
+import java.awt.Image;
+
+/**
+ * @author Thomas
+ *
+ */
+class MorphologicalFilter {
+
+	/**
+	 * 
+	 */
+	public MorphologicalFilter() {
+	}
+
+	static Image opening(Image image, int coefficient){
+		return null;
+	}
+
+	static Image closing(Image image, int coefficient){
+		return null;
+	}
+
+
+	static Image erose(Image image, int coefficient){
+		return null;
+	}
+
+	static Image dilatate(Image image, int coefficient){
+		return null;
+	}
+}
