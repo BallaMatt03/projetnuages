94bcffebda8ea5cc46dc24c226bf0aaeea128b9f
tomavron94@gmail.com <tomavron94@gmail.com@826a27cf-b9c3-140f-935a-f17cb0f93c71>
2015-02-05 20:55:24 +0000

    CheckStyle modifications, optimizations, use of ImagesUtils

--- trunk/src/server/imageprocessing/processing/Clustering.java
+++ trunk/src/server/imageprocessing/processing/Clustering.java
@@ -3,12 +3,16 @@ package server.imageprocessing.processing;
 
 import java.awt.Image;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 public final class Clustering {
 
 	
 	private Clustering() {
-		// TODO Auto-generated constructor stub
 	}
 	
 	/**
@@ -17,13 +21,32 @@ public final class Clustering {
 	 * @param dst image destination
 	 * @return picture
 	 */
-	static Image applyKmeans(String src, String dst, int nbClusters) {
+	static Image applyKmeans(String src, String dst, int nbClusters) 
+			throws IllegalArgumentException, IOException {
         // create new KMeans object 
         Kmeans kmeans = new Kmeans(); 
-        // call the function to actually start the clustering 
-        BufferedImage dstImage = kmeans.calculate(kmeans.loadImage(src), nbClusters); 
-        // save the resulting image 
-        kmeans.saveImage(dst, dstImage); 
+
+        BufferedImage img  = null;
+        try {
+        	img = ImageIO.read(new File(src));
+        } catch (IOException e ) {
+        	System.err.println("Fichier non trouvé à l'emplacement spécifié : " + src);
+        	throw new IllegalArgumentException("Le paramètre src est incorrect : " + src);
+        }
+
+        // call the function to actually start the clustering
+        BufferedImage dstImage = kmeans.calculate(img, nbClusters); 
+        
+        // save the resulting image
+        try {
+			ImageUtils.saveimageasJpeg(dstImage,  new FileOutputStream(dst), 100);
+		} catch (IOException e) {
+			if ( dst == null   || dst.isEmpty()) {
+	        	throw new IllegalArgumentException("Le paramètre dst est incorrect : " + dst);			
+			} else {
+				e.printStackTrace();
+			}
+		}
         
         return dstImage;
 	}
--- trunk/src/server/imageprocessing/processing/Kmeans.java
+++ trunk/src/server/imageprocessing/processing/Kmeans.java
@@ -1,25 +1,32 @@
 package server.imageprocessing.processing;
 
-import java.awt.image.BufferedImage; 
-import java.io.File; 
-import java.util.Arrays; 
-import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.util.Arrays;
 
 public class Kmeans{ 
     BufferedImage original; 
     BufferedImage result; 
     Cluster[] clusters; 
     
-    public Kmeans() {    } 
+    public Kmeans() {} 
      
-    public BufferedImage calculate(BufferedImage image, int k) { 
+    /**
+     * Launch the calculation.
+     * @param image
+     * 			Source Image
+     * @param coefficient
+     * 			number of clusters
+     * @return
+     * 			result image
+     */
+    public BufferedImage calculate(BufferedImage image, int coefficient) { 
         long start = System.currentTimeMillis(); 
-        int w = image.getWidth(); 
-        int h = image.getHeight(); 
+        int width = image.getWidth(); 
+        int height = image.getHeight(); 
         // create clusters 
-        clusters = createClusters(image,k); 
+        clusters = createClusters(image,coefficient); 
         // create cluster lookup table 
-        int[] lut = new int[w*h]; 
+        int[] lut = new int[width * height ]; 
         Arrays.fill(lut, -1); 
          
         // at first loop all pixels will move their clusters 
@@ -29,16 +36,16 @@ public class Kmeans{
         while (pixelChangedCluster) { 
             pixelChangedCluster = false; 
             loops++; 
-            for (int y=0;y<h;y++) { 
-                for (int x=0;x<w;x++) { 
+            for (int y = 0 ; y < height ; y++) { 
+                for (int x = 0 ; x < width ; x++) { 
                     int pixel = image.getRGB(x, y); 
                     Cluster cluster = findMinimalCluster(pixel); 
-                    if (lut[w*y+x]!=cluster.getId()) { 
+                    if (lut[ width * y + x ] != cluster.getId()) { 
                         // cluster changed 
-                        if (lut[w*y+x]!=-1) { 
+                        if (lut[ width * y + x ] != -1) { 
                             // remove from possible previous  
                             // cluster 
-                            clusters[lut[w*y+x]].removePixel( 
+                            clusters[lut[ width * y + x ]].removePixel( 
                                                         pixel); 
                         } 
                         // add pixel to cluster 
@@ -46,73 +53,71 @@ public class Kmeans{
                         // continue looping  
                         pixelChangedCluster = true; 
                         // update lut 
-                        lut[w*y+x] = cluster.getId(); 
+                        lut[ width * y + x ] = cluster.getId(); 
                     } 
                 } 
             } 
         } 
         
         // create result image 
-        BufferedImage result = new BufferedImage(w, h, BufferedImage.TYPE_BYTE_BINARY); 
+        BufferedImage result = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_BINARY); 
         
-        for (int y=0;y<h;y++) { 
-            for (int x=0;x<w;x++) { 
-                int clusterId = lut[w*y+x]; 
+        for (int y = 0 ; y < height ; y++) { 
+            for (int x  = 0 ; x < width ; x++) { 
+                int clusterId = lut[ width * y + x ]; 
                 result.setRGB(x, y, clusters[clusterId].getRGB()); 
             } 
         } 
         long end = System.currentTimeMillis(); 
-        System.out.println("Clustered to "+ k + " clusters in "+ loops  +" loops in "+(end-start)+" ms."); 
+        System.out.println("Clustered to " + coefficient + " clusters in " + loops  + " loops in " + ( end - start ) + " ms." ); 
         return result; 
     } 
      
-    public Cluster[] createClusters(BufferedImage image, int k) { 
+    /**
+     * Create a cluster.
+     * @param image
+     * 			source image
+     * @param nbClust
+     * 			coefficient
+     * @return
+     * 			array of clusters
+     */
+    public Cluster[] createClusters(BufferedImage image, int nbClust) { 
         // Here the clusters are taken with specific steps, 
         // so the result looks always same with same image. 
         // You can randomize the cluster centers, if you like. 
-        Cluster[] result = new Cluster[k]; 
-        int x = 0; int y = 0; 
-        int dx = image.getWidth()/k; 
-        int dy = image.getHeight()/k; 
-        for (int i=0;i<k;i++) { 
-            result[i] = new Cluster(i,image.getRGB(x, y)); 
-            x+=dx; y+=dy; 
+        Cluster[] result = new Cluster[nbClust]; 
+        int dx = image.getWidth() / nbClust; 
+        int dy = image.getHeight() / nbClust; 
+        int tempX = 0; 
+        int tempY = 0; 
+        for (int i = 0 ; i < nbClust; i++) { 
+            result[i] = new Cluster(i,image.getRGB(tempX, tempY)); 
+            tempX += dx;
+            tempY += dy; 
         } 
         return result; 
     } 
      
-    public Cluster findMinimalCluster(int rgb) { 
+    /**
+     * Find the minimal Cluster.
+     * @param rgb
+     * 			rgb value
+     * @return
+     * 			minimal cluster
+     */
+    private Cluster findMinimalCluster(int rgb) { 
         Cluster cluster = null; 
         int min = Integer.MAX_VALUE; 
-        for (int i=0;i<clusters.length;i++) { 
+        for (int i = 0 ; i < clusters.length ; i++) { 
             int distance = clusters[i].distance(rgb); 
-            if (distance<min) { 
+            if (distance < min) { 
                 min = distance; 
                 cluster = clusters[i]; 
             } 
         } 
         return cluster; 
-    } 
-     
-    public void saveImage(String filename, BufferedImage image) { 
-        File file = new File(filename); 
-        try { 
-            ImageIO.write(image, "png", file); 
-        } catch (Exception e) { 
-            System.out.println(e.toString()+" Image '"+filename 
-                                +"' saving failed."); 
-        } 
-    } 
-     
-    public BufferedImage loadImage(String filename) { 
-        BufferedImage result = null; 
-        try { 
-            result = ImageIO.read(new File(filename)); 
-        } catch (Exception e) { 
-            System.out.println(e.toString()+" Image '"+filename+"' not found."); 
-        } 
-        return result; 
-    } 
+    }
      
     class Cluster { 
         int id; 
@@ -153,55 +158,53 @@ public class Kmeans{
             int r = reds / pixelCount; 
             int g = greens / pixelCount; 
             int b = blues / pixelCount; 
-            if(r<220 && g<220)
-            {
+            if ( r < 220 && g < 220) {
             	r = 0;
             	g = 0;
             	b = 0;
-            }
-            else
-            {
+            } else {
             	r = 255;
             	g = 255;
             	b = 255;            	
             }
             return 0xff000000|r<<16|g<<8|b; 
         } 
+        
         void addPixel(int color) { 
             int r = color>>16&0x000000FF;  
             int g = color>> 8&0x000000FF;  
             int b = color>> 0&0x000000FF; 
-            reds+=r; 
-            greens+=g; 
-            blues+=b;
+            reds += r; 
+            greens += g; 
+            blues += b;
             pixelCount++; 
-            red   = reds/pixelCount; 
-            green = greens/pixelCount; 
-            blue  = blues/pixelCount; 
+            red   = reds / pixelCount; 
+            green = greens / pixelCount; 
+            blue  = blues / pixelCount; 
         } 
          
         void removePixel(int color) { 
             int r = color>>16&0x000000FF;  
             int g = color>> 8&0x000000FF;  
             int b = color>> 0&0x000000FF;  
-            reds-=r; 
-            greens-=g; 
-            blues-=b; 
+            reds -= r; 
+            greens -= g; 
+            blues -= b; 
             pixelCount--; 
-            red   = reds/pixelCount; 
-            green = greens/pixelCount; 
-            blue  = blues/pixelCount; 
+            red   = reds / pixelCount; 
+            green = greens / pixelCount; 
+            blue  = blues / pixelCount; 
         } 
          
         int distance(int color) { 
             int r = color>>16&0x000000FF;  
             int g = color>> 8&0x000000FF;  
-            int b = color>> 0&0x000000FF;  
-            int rx = Math.abs(red-r); 
-            int gx = Math.abs(green-g); 
-            int bx = Math.abs(blue-b); 
-            int d = (rx+gx+bx) / 3; 
-            return d; 
+            int b = color>> 0&0x000000FF;
+            
+            int rx = Math.abs( red - r ); 
+            int gx = Math.abs( green - g ); 
+            int bx = Math.abs( blue - b ); 
+            return ( rx + gx + bx ) / 3; 
         } 
     } 
      
